153114b7e74765d63ac8acb1d88f85641e7bbf5e
Erkam Çetkin <cetkinerkam17@gmail.com>
2025-06-20 02:25:05 +0300

    Generator ve Discriminator sınıfları eklendi

--- Gans_sentetik_data.py
+++ Gans_sentetik_data.py
@@ -49,12 +49,77 @@ dataLoader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 #%% Discriminator olusturalım
 
+class Discriminator(nn.Module):
+    
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        
+        self.model = nn.Sequential(
+            
+            nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 4, stride = 2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace= True),
+            
+            nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(in_channels = 256, out_channels = 1, kernel_size = 4, stride=1, padding=0),
+            nn.Sigmoid()
+            
+            
+        )
+        
+    def forward(self,x):
+        return self.model(x)
 
 
 
+#%% generator modeli 
 
+class Generator(nn.Module):
+    def __init__(self, z_dim):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            # girdi: (z_dim,) → reshape ile (z_dim, 1, 1)
+            nn.ConvTranspose2d(z_dim, 512, 4, 1, 0),    # (512, 4, 4)
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
 
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),      # (256, 8, 8)
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
 
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),      # (128, 16, 16)
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),       # (64, 32, 32)
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(64, 3, 4, 2, 1),         # (3, 64, 64)
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), x.size(1), 1, 1)  # (batch, z_dim) → (batch, z_dim, 1, 1)
+        return self.model(x)
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            )
 
 
 
